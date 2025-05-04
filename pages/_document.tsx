2df27f759944bfc776c75db300433e96833cda5a
moritam <hayato.moritan@gmail.com>
2025-05-04 09:18:38 +0800

    Change default name, etc.

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   const meta = {
     title: "Next.js Blog Starter Kit",
     description: "Clone and deploy your own Next.js portfolio in minutes.",
-    image: "https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png",
+    image: "https://my-blog-vert-theta.vercel.app/images/profile.png",
   };
 
   return (
@@ -17,7 +17,7 @@ export default function Document() {
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@moritaniam" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
---
+++ pages/posts/first-blog.md
@@ -0,0 +1,10 @@
+---
+title: 初めての自作ブログ
+date: 2024/11/7
+description: 初めて自作ブログを作ってみた。
+tag: web development
+author: moritam
+---
+
+## 初めての投稿
+テスト
